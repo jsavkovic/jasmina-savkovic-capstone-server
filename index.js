3a4887291d1c5ab3f28b279acc5aa76990a86ca0
Jasmina Savkovic <jsavkovi@gmail.com>
2024-07-03 19:21:39 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 8080
+
+app.get('/', (_req, res) => {
+    res.send(
+        'This is a homePage for Lendaroo, please make a request!'
+    )
+})
+
+app.listen(PORT, () => {
+    console.log('App is running on port ', PORT)
+})
